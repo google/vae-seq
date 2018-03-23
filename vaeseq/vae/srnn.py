d4d2e1468e51f95bcb3b4b71bc5223108e740cba
Yury Sulsky <yury.sulsky@gmail.com>
2018-03-23 01:20:59 -0400

    Move UTF-8 coding line up

--- vaeseq/vae/srnn.py
+++ vaeseq/vae/srnn.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Google, Inc.,
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# -*- coding: utf-8 -*-
 r""""SRNN as described in:
 
 Marco Fraccaro, Søren Kaae Sønderby, Ulrich Paquet, Ole Winther.
